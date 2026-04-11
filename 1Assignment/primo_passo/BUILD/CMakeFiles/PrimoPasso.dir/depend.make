a24920410e6902bcb04d361007dff4c1ab0560a1
Michael <barbi.michael3@gmail.com>
2026-04-11 15:47:31 +0200

    remove build directory

--- 1Assignment/primo_passo/BUILD/CMakeFiles/PrimoPasso.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for PrimoPasso.
-# This may be replaced when dependencies are built.
--- 1Assignment/primo_passo/BUILD/CMakeFiles/install-PrimoPasso.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-PrimoPasso"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-PrimoPasso.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
