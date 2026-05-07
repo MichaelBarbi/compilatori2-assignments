d549260bc6f34801b3142ef002ff850ed3430a98
Alessandro318290 <318290@studenti.unimore.it>
2026-05-07 15:49:14 +0200

    modify comment

--- 3Assignment/test/Loop.c
+++ 3Assignment/test/Loop.c
@@ -11,7 +11,7 @@ int loop(int a, int b, int c) {
 
   // Loop con invariant annidato
   for (i = a; i < b; i++) {
-    int w = x + y;   // loop-invariant deve essere hoistato
+    int w = x + y;   // loop-invariant: deve essere hoistato
     ret += w * i;    // NON loop-invariant (dipende da i)
   }
 
